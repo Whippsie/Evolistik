02cf9053c940d2a4d21887bf1cfebfd164813fdb
Maudee <maude.sabourin@umontreal.ca>
2017-11-03 22:03:01 -0400

    Finished p1 and p2, still working p3

---
+++ portail.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+  createChildren();
+  //doesnt work
+  for(i = 1; i <=10; i++) {
+     $('<button/>', {
+        text: "lkekelelkel", //set text 1 to 10
+        id: 'btn_'+i,
+        click: function () { alert('hi'); }
+    });
+  }
+});
+/*
+
+// 1. Create the button
+var button = document.createElement("button");
+button.innerHTML = "Do Something";
+
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+// 3. Add event handler
+button.addEventListener ("click", function() {
+  alert("did something");
+});
+*/
+function createButton(context, func){
+    var button = document.createElement("input");
+    button.type = "button";
+    button.id = "btntest";
+    button.value = "im a button";
+    button.onclick = func;
+    context.appendChild(button);
+}
+function createChildren()
+	{
+	   for(var i=0; i<30; i++)
+	   {
+	    var btn = document.createElement("button");
+    	btn.setAttribute("id","btn"+i);
+        btn.setAttribute("style","font-size:14px;background-color: #4CAF50");
+    	//btn.setAttribute("value",nextChar);
+        btn.innerHTML="a";
+    	//btn.setAttribute("text",nextChar);
+    	document.body.appendChild(btn);
+    	}
+    };
+
+
+
+
